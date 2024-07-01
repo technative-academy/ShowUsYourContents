2f86f7ca47e2a69ec658974afc5f41ff6c74cd60
Conrad G <143351775+cnrdgrgry@users.noreply.github.com>
2024-07-01 15:49:45 +0100

    Initial creation and commit of index.js:
    Instantiating express server.

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+// import characters from "./src/routes/characters.js";
+// import races from "./src/routes/races.js";
+
+const port = process.env.PORT || 3001;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const router = express.Router();
+// router.use("/characters", characters);
+// router.use("/races", races);
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Listening on port: http://localhost:${port}.`);
+});
