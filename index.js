69b7de7774a467dbdd420e4093733b3c3eca0ea1
Jo Pickering <joanna.r.pickering@gmail.com>
2024-07-02 10:25:30 +0100

    makes changes to bags route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import users from "./src/routes/users.js";
+import bags from "./src/routes/bags.js"
 
 const port = process.env.PORT ?? 3001;
 
@@ -13,6 +14,7 @@ const router = express.Router();
 app.use(router);
 
 router.use("/users", users);
+router.use("/bags", bags);
 
 app.listen(port, () => {
   console.log(`Listening on port: http://localhost:${port}.`);
---
+++ src/routes/bags.js
@@ -0,0 +1,90 @@
+import express from "express";
+import pool from "../db.js";
+const router = express.Router();
+
+//Defining routes
+
+// router.get   /users getAll
+// GET /bags - Retrieve all users
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM bags");
+    res.status(200).json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error 500: Internal server error; no bags found!");
+  }
+});
+
+//router.get   /bags/:id
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query("SELECT * FROM bags WHERE id = $1", [id]);
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error 500: Internal server error; bag not found! Please consult lost luggage...");
+  }
+});
+
+//router.post  //bags (creating a new bag)  NEEDS USER ID!!!
+
+router.post("/", async (req, res) => {
+  const { bag_name, description } = req.body;
+
+  let result;
+  try {
+    result = await pool.query(
+      "INSERT INTO bags (bag_name, description) values ($1, $2) RETURNING *;",
+      [bag_name, description]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(`DB error occurred when creating bag:\n${err}`);
+    res.status(500).json({ error: "DB error occurred when creating bag" });
+    return;
+  }
+});
+
+//router.put   //bags/:id (updated/replacing a bag)  NEEDS USER ID
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { bag_name, description } = req.body;
+
+  let result;
+  try {
+    result = await pool.query(
+      "UPDATE bags SET id = $1, bag_name = $2, description = $3 WHERE id = $5 RETURNING *;",
+      [id, bag_name, description]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(`DB error occurred when updating bag:\n${err}`);
+    res.status(500).json({ error: "DB error occurred when updating bag" });
+    return;
+  }
+});
+
+//router.delete   //bag/:id (kill) ONLY USER CAN DELETE BAG
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  let result;
+  try {
+    result = await pool.query("DELETE FROM bags WHERE id = $1 RETURNING *;", [
+      id,
+    ]);
+    res.status(201).json({ message: "Deletion successful" });
+  } catch (err) {
+    console.error(`DB error occurred when deleting bag:\n${err}`);
+    res.status(500).json({ error: "DB error occurred when deleting bag" });
+    return;
+  }
+});
+
+export default router;
